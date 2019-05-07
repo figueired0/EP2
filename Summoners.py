e21edf64d1b6868bbc537dcf8b874e9b9074f4b7
Igor Figueiredo <igor_figueired0@hotmail.com>
2019-05-07 16:49:30 -0300

    jogo base

---
+++ Summoners.py
@@ -0,0 +1,150 @@
+# Importando as bibliotecas necessárias.
+import pygame
+from os import path
+
+# Estabelece a pasta que contem as figuras e sons.
+img_dir = path.join(path.dirname(__file__), 'img')
+snd_dir = path.join(path.dirname(__file__), 'snd')
+
+# Dados gerais do jogo.
+WIDTH = 480 # Largura da tela
+HEIGHT = 600 # Altura da tela
+FPS = 60 # Frames por segundo
+
+# Define algumas variáveis com as cores básicas
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+RED = (255, 0, 0)
+GREEN = (0, 255, 0)
+BLUE = (0, 0, 255)
+YELLOW = (255, 255, 0)
+
+# Classe Jogador que representa o personagem
+class Player(pygame.sprite.Sprite):
+    
+    # Construtor da classe.
+    def __init__(self,player_img):
+        
+        # Construtor da classe pai (Sprite).
+        pygame.sprite.Sprite.__init__(self)
+        
+        # Carregando a imagem do personagem.
+        player_img = pygame.image.load(path.join(img_dir, "scooby.png")).convert()
+        self.image = player_img
+        
+        # Diminuindo o tamanho da imagem.
+        self.image = pygame.transform.scale(player_img, (50, 38))
+        
+        # Deixando transparente.
+        self.image.set_colorkey(BLACK)
+        
+        # Detalhes sobre o posicionamento.
+        self.rect = self.image.get_rect()
+        
+        # Centraliza embaixo da tela.
+        self.rect.centerx = WIDTH / 2
+        self.rect.bottom = HEIGHT - 10
+        
+        # Velocidade do boneco
+        self.speedx = 0
+        
+        #Criar um boneco
+    
+    # Metodo que atualiza a posição do boneco
+    def update(self):
+        self.rect.x += self.speedx
+        
+        # Mantem dentro da tela
+        if self.rect.right > WIDTH:
+            self.rect.right = WIDTH
+        if self.rect.left < 0:
+            self.rect.left = 0
+            
+
+# Carrega todos os assets uma vez só           
+def load_assets(img_dir, snd_dir):
+    assets={}
+    assets['player_img'] = pygame.image.load(path.join(img_dir,'scooby.png')).convert()
+    assets['background'] = pygame.image.load(path.join(img_dir,"Cenário.gif")).convert()
+
+# Inicialização do Pygame.
+pygame.init()
+pygame.mixer.init()
+
+# Tamanho da tela.
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
+
+# Nome do jogo
+pygame.display.set_caption("Summoners")
+
+# Carrega todos os assets uma vez só e guarda em um dicionario
+assets = load_assets(img_dir,snd_dir)
+
+# Variável para o ajuste de velocidade
+clock = pygame.time.Clock()
+
+# Carrega o fundo do jogo
+background = assets['background']
+background_rect = background.get_rect()
+
+# Carrega os sons do jogo
+pygame.mixer.music.load(path.join(snd_dir, 'Background_sound.ogg'))
+pygame.mixer.music.set_volume(0.4)
+
+
+# Cria uma nave. O construtor será chamado automaticamente.
+player = Player(assets['player_img'])
+
+# Cria um grupo de todos os sprites e adiciona a nave.
+all_sprites = pygame.sprite.Group()
+all_sprites.add(player)
+
+# Comando para evitar travamentos.
+try:
+    
+    # Loop principal.
+    pygame.mixer.music.play(loops=-1)
+    running = True
+    while running:
+        
+        # Ajusta a velocidade do jogo.
+        clock.tick(FPS)
+        
+        # Processa os eventos (mouse, teclado, botão, etc).
+        for event in pygame.event.get():
+            
+            # Verifica se foi fechado.
+            if event.type == pygame.QUIT:
+                running = False
+            
+            # Verifica se apertou alguma tecla.
+            if event.type == pygame.KEYDOWN:
+                # Dependendo da tecla, altera a velocidade.
+                if event.key == pygame.K_LEFT:
+                    player.speedx = -8
+                if event.key == pygame.K_RIGHT:
+                    player.speedx = 8
+                    
+            # Verifica se soltou alguma tecla.
+            if event.type == pygame.KEYUP:
+                # Dependendo da tecla, altera a velocidade.
+                if event.key == pygame.K_LEFT:
+                    player.speedx = 0
+                if event.key == pygame.K_RIGHT:
+                    player.speedx = 0
+                    
+        # Depois de processar os eventos.
+        # Atualiza a acao de cada sprite.
+        all_sprites.update()
+    
+        # A cada loop, redesenha o fundo e os sprites
+        screen.fill(BLACK)
+        screen.blit(background, background_rect)
+        all_sprites.draw(screen)
+        
+        # Depois de desenhar tudo, inverte o display.
+        pygame.display.flip()
+        
+finally:
+    
+    pygame.quit()
