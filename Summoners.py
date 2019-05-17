73dd3032fabd4ad7cec5cf2ed2a5889740653462
Igor Figueiredo <igor_figueired0@hotmail.com>
2019-05-17 08:05:59 -0300

    att

--- Summoners.py
+++ Summoners.py
@@ -25,10 +25,11 @@ YELLOW = (255, 255, 0)
 class Player(pygame.sprite.Sprite):
     
     # Construtor da classe.
-    def __init__(self,player_img):
+    def __init__(self, player_img):
         
         # Construtor da classe pai (Sprite).
         pygame.sprite.Sprite.__init__(self)
+        
         
         # Carregando a imagem do personagem.
         player_img = pygame.image.load(path.join(img_dir, "scooby.png")).convert()
@@ -47,19 +48,21 @@ class Player(pygame.sprite.Sprite):
         self.pos = vec(WIDTH /2, HEIGHT / 2)
         self.rect.center = (WIDTH / 2, HEIGHT /2)
         
-        # Velocidade
-        self.vel = vec(0, 0)
-        
         # Aceleração
         self.acc = vec(0, 0)
+        
+        # Velocidade
+        self.vel = vec(0, 0)
         
         # Propriedades dos jogadores (Movimento)
         self.PLAYER_ACC = 1
         self.PLAYER_FRICTION = -0.12
+        self.PLAYER_GRAV = 0.5
         
     # Metodo que atualiza a posição do boneco
     def update(self):
         # Gerando a graviade.
+        self.acc = vec(0 , self.PLAYER_GRAV)
         self.acc = vec(0 , 0.1)
         
         # Definindo as teclas
@@ -86,7 +89,12 @@ class Player(pygame.sprite.Sprite):
         if self.pos.y == 0:
             self.pos.y = 0
             
-        self.rect.center = self.pos
+        self.rect.midbottom = self.pos
+        
+    def jump(self):
+        # Personagem pula somente se estiver na plataforma
+        
+            self.vel.y = -20
 
 class Platform(pygame.sprite.Sprite):
     def __init__(self, x, y, w, h):
@@ -99,7 +107,6 @@ class Platform(pygame.sprite.Sprite):
         self.rect = self. image.get_rect()
         self.rect.x = x
         self.rect.y = y
-        
         
         
 # Carrega todos os assets uma vez só           
@@ -151,9 +158,14 @@ all_sprites.add(player2)
 # Cria um grupo de todos os sprites e adiciona uma plataforma.
 platforms = pygame.sprite.Group()
 
-p1 = Platform(0, HEIGHT - 1, WIDTH, 2)
+# Cria uma plataforma
+p1 = Platform(0, HEIGHT - 40, WIDTH, 40)
 all_sprites.add(p1)
+platforms.add(p1)
 
+p2 = Platform(WIDTH/2 - 50, HEIGHT * 3 / 4, 100, 20)
+all_sprites.add(p2)
+platforms.add(p2)
 
 # Comando para evitar travamentos.
 try:
@@ -174,12 +186,28 @@ try:
                 running = False
             
             # Verifica se apertou alguma tecla.
-            
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_UP:
+                    player1.jump()
+                if event.key == pygame.K_w:
+                    player2.jump()
                     
         # Depois de processar os eventos.
         # Atualiza a acao de cada sprite.
         all_sprites.update()
-    
+        hits = pygame.sprite.spritecollide(player1, platforms, False)
+        hits = pygame.sprite.spritecollide(player2, platforms, False)
+        if hits:
+            # Jogador 1 colide com a plataforma
+            player1.pos.y = hits[0].rect.top
+            # jogar 1 se mantém na plataforma
+            player1.vel.y = 0
+            
+            # Jogador 2 colide com a plataforma
+            player2.pos.y = hits[0].rect.top
+            # Jogador 2 se mantém na plataforma
+            player2.vel.y = 0
+        
         # A cada loop, redesenha o fundo e os sprites
         screen.fill(WHITE)
         screen.blit(background, background_rect)
